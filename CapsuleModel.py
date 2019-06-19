cc468b5ed669b8ad44561d3bcbb43d2a47cf61c6
zgahwuqiankun@163.com <au52185879@>
2019-06-19 22:46:42 +0800

    capsule net commit

--- CapsuleModel.py
+++ CapsuleModel.py
@@ -4,27 +4,36 @@ import numpy as np
 import pandas as pd
 from keras.models import Model
 from sklearn.model_selection import train_test_split
-from keras.layers import Conv1D,GlobalMaxPool1D,BatchNormalization,Input,Embedding,Dense,Dropout
+from keras.layers import GRU, Bidirectional,Flatten,SpatialDropout1D,Input,Embedding,Dense,Dropout
 from get_evaluate import get_evaluate
 
 
-# os.environ["CUDA_VISIBLE_DEVICES"] = "1"
-print('start', time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
+# from capsule_layer import Capsule
+from Capsule_Keras import Capsule
 
-isEnhanced = False
+
+#选择训练显卡
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+print('start', time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
 
 num_words = 80000 #字典的个数
 maxlen = 400 #每一个输入样本的长度
-
-
 kernel_size = 3
-DIM = 512  #词向量的维度
+DIM = 400  #词向量的维度
 batch_size = 256
-
+gru_len = 128
+Routings = 4
+Num_capsule = 32
+Dim_capsule = 32
+dropout_p = 0.1
+rate_drop_dense = 0.1
 
 n_start = 1
 n_end = 51
 result_list=[]
+
+
+isEnhanced = True
 
 if isEnhanced:
     fact_train = np.load('./data_enhanced/enhanced_data/enhanced_data_train_fact_pad_seq_80000_400.npy')
@@ -40,8 +49,6 @@ labels_test =np.load('./data_preprocessing/data_deal/data_model_use/labels/data_
 fact_valid =np.load('./data_preprocessing/data_deal/data_model_use/fact/data_valid_fact_pad_seq_80000_400.npy')
 labels_valid =np.load('./data_preprocessing/data_deal/data_model_use/labels/data_valid_labels_accusation.npy')
 
-
-
 def get_model():
     data_input = Input(shape=[fact_train.shape[1]])
     word_vec = Embedding(input_dim=num_words + 1,
@@ -49,17 +56,20 @@ def get_model():
                          output_dim=DIM,
                          mask_zero=0,
                          name='Embedding')(data_input)
-    x = word_vec
-    x = Conv1D(filters=512, kernel_size=[kernel_size], strides=1, padding='same', activation='relu')(x)
-    x = GlobalMaxPool1D()(x)
-    x = BatchNormalization()(x)
-    x = Dense(1000, activation="relu")(x)
-    x = Dropout(0.2)(x)
-    x = Dense(labels_train.shape[1], activation="sigmoid")(x)
-    model = Model(inputs=data_input, outputs=x)
-    model.compile(loss='binary_crossentropy',
-                  optimizer='adam',
-                  metrics=['accuracy'])
+    x = SpatialDropout1D(rate_drop_dense)(word_vec)
+    x = Bidirectional(GRU(gru_len, activation='relu', dropout=dropout_p, recurrent_dropout=dropout_p, return_sequences=True))(x)
+    # x = Capsule(num_capsule=Num_capsule, dim_capsule=Dim_capsule, routings=Routings, share_weights=True)(x)
+    x = Capsule(Num_capsule, Dim_capsule, Routings, True)(x)
+    x = Flatten()(x)
+    x = Dropout(dropout_p)(x)
+    x = Dense(500, activation='sigmoid')(x)
+    x = Dense(300, activation='sigmoid')(x)
+    output = Dense(202, activation='sigmoid')(x)
+    model = Model(inputs=data_input, outputs=output)
+    model.compile(
+        loss='binary_crossentropy',
+        optimizer='adam',
+        metrics=['accuracy'])
     model.summary()
     return model
 
@@ -67,21 +77,24 @@ def get_model():
 if __name__ == "__main__":
     model = get_model()
     for i in range(n_start, n_end):
-        model.fit(x=fact_train, y=labels_train, batch_size=batch_size,validation_data=(fact_valid,labels_valid), epochs=1, verbose=1)
+        model.fit(x=fact_train, y=labels_train, batch_size=batch_size, epochs=1, verbose=1)
         # model.fit(x=fact_test, y=labels_test, batch_size=batch_size, epochs=1, verbose=1)
+        model.save('./kerasmodel/%d_%d/Capsule/Capsule_epochs_%d_4route.h5' % (num_words, maxlen, i))
         if isEnhanced:
-            model.save('./model_save/CNN_Enhanced/CNN_epochs_%d.h5' % i)
+            model.save('./model_save/Capsule_Enhanced/CNN_epochs_%d.h5' % i)
         else:
-            model.save('./model_save/CNN_No_Enhanced/CNN_epochs_%d.h5' % i)
+            model.save('./model_save/Capsule_No_Enhanced/CNN_epochs_%d.h5' % i)
+
+
         y = model.predict(fact_test[:])
         print('第%s次迭代测试结果如下：' % i)
         #获取测试集结果
         rs = get_evaluate(y_pred=y, y_true=labels_test, type="top")
-        result_list.append(rs.get_all_evaluate())
+        result_list.append(rs.get_all_result())
         print(pd.DataFrame(result_list, columns=["accu", "pre_micro", "recall_micro", "f1_micro","pre_macro","recall_macro","f1_macro"]).to_string(index=True))
 
     df =pd.DataFrame(result_list,columns=["accu", "pre_micro", "recall_micro", "f1_micro","pre_macro","recall_macro","f1_macro"])
     nowtime = time.strftime("%Y%m%d%H%M%S", time.localtime())
-    df.to_csv("./model_save/result_csv/CNN"+ nowtime + ".csv", index=True, encoding="utf_8_sig", mode="a", header=True)
+    df.to_csv("./model_save/result_csv/Capsule"+ nowtime + ".csv", index=True, encoding="utf_8_sig", mode="a", header=True)
     print('end', time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
     print('###################################################################################################################\n')
