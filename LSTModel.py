ab2e9000f208c404d05fd7c8f2b080d261268ef4
zgahwuqiankun@163.com <au52185879@>
2019-06-23 19:58:34 +0800

    lstm

--- LSTModel.py
+++ LSTModel.py
@@ -11,7 +11,7 @@ from get_evaluate import get_evaluate
 # os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 print('start', time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
 
-isEnhanced = False
+isEnhanced = True
 
 num_words = 80000 #字典的个数
 maxlen = 400 #每一个输入样本的长度
@@ -22,7 +22,7 @@ batch_size = 256
 
 
 n_start = 1
-n_end = 11
+n_end = 21
 result_list=[]
 
 if isEnhanced:
