c4b81e835966e515333d1d76fce8a89f5334bb12
zgahwuqiankun@163.com <au52185879@>
2019-06-23 17:16:51 +0800

    svm  data preprocessing

--- data_preprocessing/svm_data_deal/svm_data.py
+++ data_preprocessing/svm_data_deal/svm_data.py
@@ -9,11 +9,14 @@ original_dataname = "data_train"
 data_transform = data_transform()
 
 # 读取json文件,1710857行
-data_transform.read_data(path="./data_original/"+ original_dataname +".json")
+data_transform.read_data(path="../data_original/"+ original_dataname +".json")
 
 data_transform.extract_data(name='fact')
 
+originaldatanum = len(data_transform.extraction["fact"])
+
 texts = data_transform.extraction['fact'][:]
 fact_cut = data_transform.cut_texts(texts=texts, word_len=1, need_cut=True)
-with open('./fact_cut/' + original_dataname + '.pkl', mode='wb') as f:
+with open('./fact_cut/' + original_dataname + 'num_%d.pkl'% originaldatanum, mode='wb') as f:
     pickle.dump(fact_cut,f)
+print("finish %d sample"%originaldatanum)
