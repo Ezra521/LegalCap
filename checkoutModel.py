4a2c31f33765260c092fab92b1c2de9892120579
zgahwuqiankun@163.com <au52185879@>
2019-06-25 16:06:19 +0800

    add checkoutModel.py

--- checkoutModel.py
+++ checkoutModel.py
@@ -2,12 +2,11 @@ from keras.models import load_model
 import numpy as np
 from get_evaluate import get_evaluate
 import pandas as pd
-
 result_list=[]
 fact_test =np.load('./data_preprocessing/data_deal/data_model_use/fact/data_test_fact_pad_seq_80000_400.npy')
 labels_test =np.load('./data_preprocessing/data_deal/data_model_use/labels/data_test_labels_accusation.npy')
 
-model = load_model('./model_save/FastText_No_Enhanced/FastText_epochs_1.h5')
+model = load_model('./model_save/FastText_No_Enhanced/FastText_epochs_10.h5')
 
 y = model.predict(fact_test[:])
 rs = get_evaluate(y_pred=y, y_true=labels_test, type="top")
