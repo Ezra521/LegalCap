1300bb02b1c930b6d7c46f6e839ae55c373f5dd2
zgahwuqiankun@163.com <au52185879@>
2019-06-26 13:31:03 +0800

    updata make multi label data

--- multi_label/make_multi_label_data_index.py
+++ multi_label/make_multi_label_data_index.py
@@ -28,29 +28,8 @@ datalabel = data_transform.extraction['accusation']
 class_name = []
 class_index = []
 
+clase_num_index = {i:[] for i in range(1,14)}
+
+#按照标签的个数取得所有的个数标签对应样本的index存在字典里面备用
 for n, i in enumerate(datalabel):
-    # 只取罪名只有一个的样本  然后 获取罪名 和索引 索引就是第几个
-    if len(i) == 1:
-        class_name.append(i[0])
-        class_index.append(n)
-
-# 把罪名变成array数组
-class_name_array = np.array(class_name)
-# 把索引变成array数组
-class_index_array = np.array(class_index)
-
-accusation_set = list(set(class_name))
-
-accu_num = {}
-
-for i in accusation_set:
-    accu_num[i] = class_name.count(i)
-    class_index_i = class_index_array[class_name_array == i]
-    np.save('./all_accu_num_index/%d_%s.npy'%(accu_num[i],i),class_index_i)
-
-
-# print(accu_num)
-#
-# a = sorted(accu_num.items(), key=lambda x: x[1], reverse=True)
-# for i in a:
-#     print(i)
+    clase_num_index[len(i)].append(n)
